dc124d42ab8fedb8ab2234625ff8ce90c42e0c91
Martyna <martyna.sowinska@hotmail.com>
2021-07-08 16:56:44 +0200

    Button for submitting teammates

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-let arrayOfPeople = [
-    {
-        "Name": "",
-        "Surname": ""
+let fname = []
+let lname = []
 
-    }
-]
+let fnameInput = document.getElementById("fname").nodeValue
+let lnameInput = document.getElementById("lname").nodeValue
+
+let messageBox = document.getElementById("display")
+
+function insert(){
+    fname.push(fnameInput.nodeValue)
+    lname.push(lnameInput.nodeValue)
+
+    clearAndShow()
+}
+
+function clearAndShow(){
+
+    fnameInput.nodeValue = ""
+    lnameInput.nodeValue = ""
+
+    messageBox.innerHTML = ""
+
+    messageBox.innerHTML += "First name:" + fname.join(", ") + "<br/>"
+    messageBox.innerHTML += "Last name:" + lname.join(", ") + "<br/>"
+    
+}
